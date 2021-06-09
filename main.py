23814a667fef0538eb2f7101fca7852a9cb944fb
Tousif Kabir <84358559+ExoticKabir@users.noreply.github.com>
2021-06-09 07:44:14 +0000

    Version 4 of my program deals with the issue of version 3. It fixes the problem where there was no end-screen and you can just spam the correct answer to the last question and get an indefinite amount of points. Although this isn't the best end-screen, it is a solution. It also adds a quit button to the quiz class so that they user can exit at any time. Design is yet to be applied.

--- main.py
+++ main.py
@@ -126,14 +126,14 @@ class Introduction:
         #Label widget for our heading
         self.heading_label = Label(self.quiz_frame,
                                    text = "This test will test your math skills.",
-                                   font=("Helvetica", "13"),
+                                   font=("Comic Sans MS", "13"),
                                    bg=background_color)
         self.heading_label.grid(row=0, padx=20)
 
         #creates a label to ask for the name
         self.user_label = Label(self.quiz_frame,
                                 text="Enter your name to begin this journey...",
-                                font=("Helvetica", "13"),
+                                font=("Comic Sans MS", "13"),
                                 bg=background_color)
         self.user_label.grid(row=1, padx=20, pady=20)
 
@@ -148,6 +148,7 @@ class Introduction:
                                       bg="SpringGreen2",
                                       command=self.name_collection)
         self.continue_button.grid(row=3, padx=20, pady=20)
+
 
     def name_collection(self):
         name = self.entry_box.get()  #this will collect the name from the entry box
@@ -170,7 +171,7 @@ class Quiz:
         #Label widget for our question
         self.question_label = Label(self.quiz_frame,
                                    text = questions_answers[qnum][0],
-                                   font=("Helvetica", "16"),
+                                   font=("Comic Sans MS", "16"),
                                    bg=background_color)
         self.question_label.grid(row=0, padx=150)
 
@@ -180,7 +181,7 @@ class Quiz:
         #radio button 1, in order to hold first choice
         self.rb1 = Radiobutton(self.quiz_frame,
                                text=questions_answers[qnum][1],
-                               font=("Helvetica", "12"),
+                               font=("Comic Sans MS", "12"),
                                bg=background_color,
                                value=1,
                                padx=10,
@@ -194,7 +195,7 @@ class Quiz:
         #radio button 2, in order to hold first choice
         self.rb2 = Radiobutton(self.quiz_frame,
                                text=questions_answers[qnum][2],
-                               font=("Helvetica", "12"),
+                               font=("Comic Sans MS", "12"),
                                bg=background_color,
                                value=2,
                                padx=10,
@@ -208,7 +209,7 @@ class Quiz:
         #radio button 3, in order to hold first choice
         self.rb3 = Radiobutton(self.quiz_frame,
                                text=questions_answers[qnum][3],
-                               font=("Helvetica", "12"),
+                               font=("Comic Sans MS", "12"),
                                bg=background_color,
                                value=3,
                                padx=10,
@@ -222,7 +223,7 @@ class Quiz:
         #radio button 1, in order to hold first choice
         self.rb4 = Radiobutton(self.quiz_frame,
                                text=questions_answers[qnum][4],
-                               font=("Helvetica", "12"),
+                               font=("Comic Sans MS", "12"),
                                bg=background_color,
                                value=4,
                                padx=10,
@@ -247,6 +248,15 @@ class Quiz:
                                     bg=background_color)
         self.score_label.grid(row=7, padx=10, pady=1)
 
+        self.quit = Button(self.quiz_frame, 
+                                    text = "Quit",
+                                    font = ("Comic Sans MS", 13), 
+                                    bg = "red2", 
+                                    command = self.end_screen)
+        self.quit.grid(row = 7, column = 3, sticky = E, padx = 5, pady = 5)
+
+
+
     #Method for the appearance of the next question
     def questions_setup(self):
         randomiser()
@@ -266,13 +276,30 @@ class Quiz:
         if len(asked) > 9: #If this is the last question
             if choice == questions_answers[qnum][6]: #If they select the correct choice for last question
                 score += 1 # Adding one point to the score
-                scr_label.configure(text = score)
-                self.confirm_button.config(text = "Confirm")
+                self.confirm_button.destroy()
+                self.question_label.destroy()
+                self.rb1.destroy()
+                self.rb2.destroy()
+                self.rb3.destroy()
+                self.rb4.destroy()
+                
+                if score == 1: #singular grammar, 1 question, no "s" after question
+                    scr_label.configure(text = "Well done! You finished my quiz! You correctly answered " + str(score) + " question.")
+                    
+                else: #plural, correctly answered more than 1 question. So "s" after question
+                    scr_label.configure(text = "Well done! You finished my quiz! You correctly answered " + str(score) + " questions.")
+                
                 
             else: #If they select the incorrect choice for last question
                 score +=0
-                scr_label.configure(text = "The answer to the previous question was " + questions_answers[qnum][5])
-                self.confirm_button.config(text = "Confirm")    
+                scr_label.configure(text = "The answer to the previous question was '" + questions_answers[qnum][5] + "' .That was the last question so you have now finished my quiz!")
+                self.confirm_button.destroy()
+                self.question_label.destroy()
+                self.rb1.destroy()
+                self.rb2.destroy()
+                self.rb3.destroy()
+                self.rb4.destroy()
+
 
         else: #If it is not the last question
             if choice == 0: #boundary testing <- ensuring the user actually selects a choice
@@ -282,15 +309,52 @@ class Quiz:
             else: #If it isn't the last question and they HAVE selected a choice.
                 if choice == questions_answers[qnum][6]: #If they selected the correct choice
                     score += 1
-                    scr_label.configure(text = score)
+
+                    if score == 1: #singular grammar, 1 question, no "s" after question
+                        scr_label.configure(text = "Well done! You have now correctly answered " + str(score) + " question.")
+                    
+                    else: #plural, correctly answered more than 1 question. So "s" after question
+                        scr_label.configure(text = "Well done! You have now correctly answered " + str(score) + " questions.")
+                    
                     self.confirm_button.config(text = "Confirm")
                     self.questions_setup() #Will move to the next question
                 
                 else: #If the choice is incorrect 
                     score += 0
-                    scr_label.configure(text = "The answer to the previous question was " + questions_answers[qnum][5])
+                    scr_label.configure(text = "The answer to the previous question was '" + questions_answers[qnum][5] + "'.")
                     self.confirm_button.config(text = "Confirm")
-                    self.questions_setup() #Will move to the next question  
+                    self.questions_setup() #Will move to the next question
+
+
+    def end_screen(self):
+        root.withdraw()
+        open_end_screen = End()
+    
+
+
+
+class End:
+    def __init__(self):
+            background = "plum1"
+            self.end_box = Toplevel(root) #Top level widgets work as windows that are directly managed by the window manager
+            self.end_box.title("End Box")
+
+            self.end_frame = Frame(self.end_box, width = 1000, height = 1000, bg = background)
+            self.end_frame.grid()
+
+            end_heading = Label(self.end_frame, text = "   Well Done! Thanks for playing   ", font = ("Tw Cen MT", 22), bg = background, pady = 15)
+            end_heading.grid(row = 0)
+
+            exit_button = Button(self.end_frame, text = "Exit", width = 10, bg = "Red", font=("Tw Cen MT", 12), command = self.close_end)
+            exit_button.grid(row=4, pady=20)
+
+    def close_end(self):
+        self.end_box.destroy()
+        root.destroy()
+
+    
+        
+    
 
 
 
